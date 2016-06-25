3ef711ee7ffac9ae65aa16c5fee3da4154530435
Iuro Nascimento <iurobpn@gmail.com>
2016-06-25 12:37:55 -0300

    Adicionado um namespace para os objetos do scatter plot

--- main.js
+++ main.js
@@ -76,9 +76,11 @@ function handle_scatter_data(data) {
 }
 
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+/* Scatter plot */
+var scatter = {}
+scatter.margin = {top: 20, right: 20, bottom: 30, left: 40};
+scatter.width = 960 - scatter.margin.left - scatter.margin.right;
+scatter.height = 500 - scatter.margin.top - scatter.margin.bottom;
 
 /*
  * value accessor - returns the value to encode for a given data object.
@@ -86,32 +88,31 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
  * map function - maps from data value to display value
  * axis - sets up axis
  */
-
 // setup x
-var xValue = function(d) { return d.x;}, // data -> value
-    xScale = d3.scale.linear().range([0, width]), // value -> display
-    xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+scatter.xValue = function(d) { return d.x;}; // data -> value
+scatter.xScale = d3.scale.linear().range([0, scatter.width]); // value -> display
+scatter.xMap = function(d) { return scatter.xScale(scatter.xValue(d));}; // data -> display
+scatter.xAxis = d3.svg.axis().scale(scatter.xScale).orient("bottom");
 
 // setup y
-var yValue = function(d) { return d.y;}, // data -> value
-    yScale = d3.scale.linear().range([height, 0]), // value -> display
-    yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
+scatter.yValue = function(d) { return d.y;}; // data -> value
+    scatter.yScale = d3.scale.linear().range([scatter.height, 0]); // value -> display
+    scatter.yMap = function(d) { return scatter.yScale(scatter.yValue(d));}; // data -> display
+    scatter.yAxis = d3.svg.axis().scale(scatter.yScale).orient("left");
 
 // setup fill color
-var cValue = function(d) { return d.epoch ;},
-    color = d3.scale.category10();
+scatter.cValue = function(d) { return d.epoch ;},
+    scatter.color = d3.scale.category10();
 
 // add the graph canvas to the body of the webpage
-var svg = d3.select("div.scatter").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+scatter.svg = d3.select("div.scatter").append("svg")
+    .attr("width", scatter.width + scatter.margin.left + scatter.margin.right)
+    .attr("height", scatter.height + scatter.margin.top + scatter.margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + scatter.margin.left + "," + scatter.margin.top + ")");
 
 // add the tooltip area to the webpage
-var tooltip = d3.select("body").append("div")
+scatter.tooltip = d3.select("body").append("div")
     .attr("class", "tooltip_scatter")
     .style("opacity", 0);
 
@@ -119,33 +120,33 @@ var tooltip = d3.select("body").append("div")
 d3.csv("scatter_data.csv", function(error, data) {
 	handle_scatter_data(data);
   // change string (from CSV) into number format
-  var x_max = d3.max(data, xValue);
-  var x_min = d3.min(data, xValue);
-  var y_max = d3.max(data, yValue);
-  var y_min = d3.min(data, yValue);
+  // scatter.x_max = d3.max(data, scatter.xValue);
+  // scatter.x_min = d3.min(data, scatter.xValue);
+  // scatter.y_max = d3.max(data, scatter.yValue);
+  // scatter.y_min = d3.min(data, scatter.yValue);
 
-  var margin_svg = 5;
+  scatter.margin_svg = 5;
   // don't want dots overlapping axis, so add in buffer to data domain
-  xScale.domain([d3.min(data, xValue)-margin_svg, d3.max(data, xValue)+margin_svg]);
-  yScale.domain([d3.min(data, yValue)-margin_svg, d3.max(data, yValue)+margin_svg]);
+  scatter.xScale.domain([d3.min(data, scatter.xValue)-scatter.margin_svg, d3.max(data, scatter.xValue)+scatter.margin_svg]);
+  scatter.yScale.domain([d3.min(data, scatter.yValue)-scatter.margin_svg, d3.max(data, scatter.yValue)+scatter.margin_svg]);
 
   // x-axis
-  svg.append("g")
+  scatter.svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+      .attr("transform", "translate(0," + scatter.height + ")")
+      .call(scatter.xAxis)
     .append("text")
       .attr("class", "label")
-      .attr("x", width)
+      .attr("x", scatter.width)
       .attr("y", -6)
       .style("text-anchor", "end")
       .text("x");
 
 
   // y-axis
-  svg.append("g")
+  scatter.svg.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
+      .call(scatter.yAxis)
     .append("text")
       .attr("class", "label")
       .attr("transform", "rotate(-90)")
@@ -155,52 +156,52 @@ d3.csv("scatter_data.csv", function(error, data) {
       .text("y");
 
   // draw dots
-  svg.selectAll(".dot")
+  scatter.svg.selectAll(".dot")
       .data(data)
     .enter().append("circle")
       .attr("class", "dot")
       .attr("r", 3.5)
-      .attr("cx", xMap)
-      .attr("cy", yMap)
-      .style("fill", function(d) { return color(cValue(d));})
+      .attr("cx", scatter.xMap)
+      .attr("cy", scatter.yMap)
+      .style("fill", function(d) { return scatter.color(scatter.cValue(d));})
       .on("mouseover", function(d) {
-          tooltip.transition()
+          scatter.tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html(d.author + "<br/> Epoch: " + d.epoch)
+          scatter.tooltip.html(d.author + "<br/> Epoch: " + d.epoch)
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
       })
       .on("mouseout", function(d) {
-          tooltip.transition()
+          scatter.tooltip.transition()
                .duration(500)
                .style("opacity", 0);
       });
 
   // d3.selectAll("p")
-	//   .data([x_min,x_max,y_min,y_max])
+	//   .data([scatter.x_min,scatter.x_max,scatter.y_min,scatter.y_max])
 	//   .enter()
 	//   .append("p")
 	//   .text(function(d) { return d;})
 	//   .exit();
 
   //draw legend
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
+  scatter.legend = scatter.svg.selectAll(".legend")
+      .data(scatter.color.domain())
     .enter().append("g")
       .attr("class", "legend")
       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
   // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", width - 18)
+  scatter.legend.append("rect")
+      .attr("x", scatter.width - 18)
       .attr("width", 18)
       .attr("height", 18)
-      .style("fill", color);
+      .style("fill", scatter.color);
 
   // draw legend text
-  legend.append("text")
-      .attr("x", width - 24)
+  scatter.legend.append("text")
+      .attr("x", scatter.width - 24)
       .attr("y", 9)
       .attr("dy", ".35em")
       .style("text-anchor", "end")
