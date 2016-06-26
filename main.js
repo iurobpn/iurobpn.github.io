027356f1dc3d685e478082e0522827a02b16ef17
Iuro Nascimento <iurobpn@gmail.com>
2016-06-26 20:55:02 -0300

    Ultimas correcoes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var scatter = {}
 var posneg = {}
 var dict = {}
 var authors = [];
+// var data_posneg = [];
 
 function capFirstChar(s) {
 	return s.charAt(0).toUpperCase() + s.slice(1);
@@ -247,19 +248,83 @@ function pre_fingerprint(local) {
 }
 
 function update_fingerprint(author) {
-	epoch = author.epoch;
-	// X = Object.keys(data);
-	delete author.epoch;
-	nome = author.author;
-	delete author.author;
-	delete author.x;
-	delete author.y;
+	data = {};
+	data.positive = author.positive;
+	data.negative = author.negative;
+	data.anger_emolex = author.anger_emolex;
+	data.anticipation = author.anticipation;
+	data.disgust = author.disgust;
+	data.fear = author.fear;
+	data.joy = author.joy;
+	data.sadness = author.sadness;
+	data.surprise = author.surprise;
+	data.trust = author.trust;
+	data.affect = author.affect;
+	data.posemo = author.posemo;
+	data.negemo = author.negemo;
+	data.anx = author.anx;
+	data.anger = author.anger;
+	data.sad = author.sad;
+	data.social = author.social;
+	data.family = author.family;
+	data.friend = author.friend;
+	data.female = author.female;
+	data.male = author.male;
+	data.cogproc = author.cogproc;
+	data.insight = author.insight;
+	data.cause = author.cause;
+	data.discrep = author.discrep;
+	data.tentat = author.tentat;
+	data.certain = author.certain;
+	data.differ = author.differ;
+	data.percept = author.percept;
+	data.see = author.see;
+	data.hear = author.hear;
+	data.feel = author.feel;
+	data.bio = author.bio;
+	data.body = author.body;
+	data.health = author.health;
+	data.sexual = author.sexual;
+	data.ingest = author.ingest;
+	data.drives = author.drives;
+	data.affiliation = author.affiliation;
+	data.achiev = author.achiev;
+	data.power = author.power;
+	data.reward = author.reward;
+	data.risk = author.risk;
+	data.focuspast = author.focuspast;
+	data.focuspresent = author.focuspresent;
+	data.focusfuture = author.focusfuture;
+	data.relativ = author.relativ;
+	data.motion = author.motion;
+	data.space = author.space;
+	data.time = author.time;
+	data.work = author.work;
+	data.leisure = author.leisure;
+	data.home = author.home;
+	data.money = author.money;
+	data.relig = author.relig;
+	data.death = author.death;
+	data.informal = author.informal;
+	data.swear = author.swear;
+	data.netspeak = author.netspeak;
+	data.assent = author.assent;
+	data.nonflu = author.nonflu;
+	data.filler = author.filler;
+	var epoch = author.epoch;
+	var nome = author.author;
+	// epoch = author.epoch;
+	// delete author.epoch;
+	// nome = author.author;
+	// delete author.author;
+	// delete author.x;
+	// delete author.y;
 	fingerprint.svg.select("text.label_fingerprint").text(nome);
 
-	features_name = Object.keys(author);
-	features_value = Object.values(author);
-	min_feature = d3.min(features_value);
-	max_feature = d3.max(features_value);
+	features_name = Object.keys(data);
+	features_value = Object.values(data);
+	var min_feature = d3.min(features_value);
+	var max_feature = d3.max(features_value);
 	
 	fingerprint.xScale.domain(features_name);
 	fingerprint.yScale.domain([min_feature, max_feature]);
@@ -284,16 +349,16 @@ function update_fingerprint(author) {
 		// .style("text-anchor", "end")
 		// .text("Value");
 
-	fingerprint.features = [];
+	var features = [];
 	for (i = 0; i < features_name.length; i++) { 
-		fingerprint.features[i] = {};
-		fingerprint.features[i].name = features_name[i];
-		fingerprint.features[i].value = features_value[i];
+		features[i] = {};
+		features[i].name = features_name[i];
+		features[i].value = features_value[i];
 	}
 
 
 	// THIS IS THE ACTUAL WORK!
-	var bars = fingerprint.svg.selectAll(".bar").data(fingerprint.features) // (data) is an array/iterable thing, second argument is an ID generator function
+	var bars = fingerprint.svg.selectAll(".bar").data(features) // (data) is an array/iterable thing, second argument is an ID generator function
 
 	bars.exit()
 	.transition()
@@ -332,13 +397,73 @@ function update_fingerprint(author) {
 
 /* Fingerprint bar chart */
 function plot_fingerprint(author) {
-	epoch = author.epoch;
-	// X = Object.keys(data);
-	delete author.epoch;
-	nome = author.author;
-	delete author.author;
-	delete author.x;
-	delete author.y;
+	data = {};
+	data.positive = author.positive;
+	data.negative = author.negative;
+	data.anger_emolex = author.anger_emolex;
+	data.anticipation = author.anticipation;
+	data.disgust = author.disgust;
+	data.fear = author.fear;
+	data.joy = author.joy;
+	data.sadness = author.sadness;
+	data.surprise = author.surprise;
+	data.trust = author.trust;
+	data.affect = author.affect;
+	data.posemo = author.posemo;
+	data.negemo = author.negemo;
+	data.anx = author.anx;
+	data.anger = author.anger;
+	data.sad = author.sad;
+	data.social = author.social;
+	data.family = author.family;
+	data.friend = author.friend;
+	data.female = author.female;
+	data.male = author.male;
+	data.cogproc = author.cogproc;
+	data.insight = author.insight;
+	data.cause = author.cause;
+	data.discrep = author.discrep;
+	data.tentat = author.tentat;
+	data.certain = author.certain;
+	data.differ = author.differ;
+	data.percept = author.percept;
+	data.see = author.see;
+	data.hear = author.hear;
+	data.feel = author.feel;
+	data.bio = author.bio;
+	data.body = author.body;
+	data.health = author.health;
+	data.sexual = author.sexual;
+	data.ingest = author.ingest;
+	data.drives = author.drives;
+	data.affiliation = author.affiliation;
+	data.achiev = author.achiev;
+	data.power = author.power;
+	data.reward = author.reward;
+	data.risk = author.risk;
+	data.focuspast = author.focuspast;
+	data.focuspresent = author.focuspresent;
+	data.focusfuture = author.focusfuture;
+	data.relativ = author.relativ;
+	data.motion = author.motion;
+	data.space = author.space;
+	data.time = author.time;
+	data.work = author.work;
+	data.leisure = author.leisure;
+	data.home = author.home;
+	data.money = author.money;
+	data.relig = author.relig;
+	data.death = author.death;
+	data.informal = author.informal;
+	data.swear = author.swear;
+	data.netspeak = author.netspeak;
+	data.assent = author.assent;
+	data.nonflu = author.nonflu;
+	data.filler = author.filler;
+	var epoch = author.epoch;
+	var nome = author.author;
+
+
 	// fingerprint.svg.select("label_fingerprint").text(nome);
 	fingerprint.svg
 		.append("text")
@@ -349,8 +474,8 @@ function plot_fingerprint(author) {
 		.style("font-size", "12px") 
 		.text(nome);
 
-	features_name = Object.keys(author);
-	features_value = Object.values(author);
+	features_name = Object.keys(data);
+	features_value = Object.values(data);
 	min_feature = d3.min(features_value);
 	max_feature = d3.max(features_value);
 	
@@ -380,16 +505,16 @@ function plot_fingerprint(author) {
 		// .style("text-anchor", "end")
 		// .text("Value");
 
-	fingerprint.features = [];
+	var features = [];
 	for (i = 0; i < features_name.length; i++) { 
-		fingerprint.features[i] = {};
-		fingerprint.features[i].name = features_name[i];
-		fingerprint.features[i].value = features_value[i];
+		features[i] = {};
+		features[i].name = features_name[i];
+		features[i].value = features_value[i];
 	}
 
 
 	// THIS IS THE ACTUAL WORK!
-	var bars = fingerprint.svg.selectAll(".bar").data(fingerprint.features) // (data) is an array/iterable thing, second argument is an ID generator function
+	var bars = fingerprint.svg.selectAll(".bar").data(features) // (data) is an array/iterable thing, second argument is an ID generator function
 
 	bars.exit()
 	.transition()
@@ -461,51 +586,82 @@ function pre_posneg(local) {
 	  posneg.color.domain(["Sadness", "Negative",  "Positive", "Joy"]);
 }
 
-function plot_posneg() {
+function handle_posneg_data(data) {
+	data.forEach(function(d) {
+		d.affect = +d.affect;
+		d.negative = +d.negative;
+		d.positive = +d.positive;
+		d.neutral = +d.neutral;
+		d.joy = +d.joy;
+		d.sadness = +d.sadness;
+		// calc percentages
+		// fd.neutral = (d.sadness + d.joy + d.positive + d.negative)/4;
+		d.neutral = 100 - d.affect;
+		d.neutral = d.neutral;
+		d.N = d.sadness + d.neutral + d.joy + d.positive + d.negative;
+		d.Sadness = d.sadness*100/(d.N - d.neutral);
+		d.Negative = d.negative*100/(d.N - d.neutral);
+		// d["Neutral"] = +d["neutral"]*100/d.N;
+		d.Positive = d.positive*100/(d.N - d.neutral);
+		d.Joy = d.joy*100/(d.N - d.neutral);
+		var x0 = -1*( d.Negative + d.Sadness);
+		var idx = 0;
+		d.negative = d.negative;
+		d.positive = d.positive;
+		d.neutral = d.neutral;
+		d.joy = d.joy;
+		
+		d.boxes = posneg.color.domain().map(function(name) { return {
+			joy: d.joy,
+			sadness: d.sadness,
+			positive: d.positive,
+			positive: d.positive,
+			negative: d.negative,
+			author: d.author,
+			name: name,
+			x0: x0,
+			x1: x0 += +d[name],
+			N: +d.N- d.neutral,
+			// n: +d[idx += 1]
+		}; });
+	});
 
-	 d3.csv("visu4.csv", function(error, data)  {
+}
 
-	  data.forEach(function(d) {
-	    // calc percentages
-	    d["Sadness"] = +d["sadness"]*100/(d.N - d["neutral"]);
-	    d["Negative"] = +d["negative"]*100/(d.N - d["neutral"]);
-	    // d["Neutral"] = +d["neutral"]*100/d.N;
-	    d["Positive"] = +d["positive"]*100/(d.N - d["neutral"]);
-	    d["Joy"] = +d["joy"]*100/(d.N - d["neutral"]);
-	    var x0 = -1*( d["Negative"] + d["Sadness"]);
-	    var idx = 0;
-	    d.boxes = posneg.color.domain().map(function(name) { return {
-	      joy: d.joy,
-	      sadness: d.sadness,
-	      positive: d.positive,
-	      positive: d.positive,
-	      negative: d.negative,
-	      author: d.author,
-	      name: name,
-	      x0: x0,
-	      x1: x0 += +d[name],
-	      N: +d.N- d["neutral"],
-	      n: +d[idx += 1]
-	    }; });
-	  });
 
-	  var min_val = d3.min(data, function(d) {
-		  return d.boxes["0"].x0;
-		  });
+function check_author(author,rem) {
+	// d3.selectAll("input").filter("#"+author).attr("checked",rem);
+	d3.selectAll("input").filter("#"+author).node().checked = rem;
+}
 
-	  var max_val = d3.max(data, function(d) {
-		  return d.boxes["3"].x1;
-		  });
+function get_check_author(author) {
+	return d3.selectAll("input").filter("#"+author).attr("checked");
+}
 
-	  posneg.xScale.domain([min_val, max_val]).nice();
-	  posneg.yScale.domain(data.map(function(d) { return d.author; }));
+function plot_posneg(data) {
+
+	// d3.csv("visu4.csv", function(error, data)  {
+	// handle_posneg_data(data);
+
+	var min_val = d3.min(data, function(d) {
+		return d.boxes["0"].x0;
+	});
+
+	var max_val = d3.max(data, function(d) {
+		return d.boxes["3"].x1;
+	});
+
+	for (i=0; i<data.length; i++) {
+		check_author(data[i].author,true);
+	}
+
+	posneg.xScale.domain([min_val, max_val]).nice();
+	posneg.yScale.domain(data.map(function(d) { return d.author; }));
 
 
 	posneg.tooltip = d3.select("body").append("div")
 	    .attr("class", "tooltip tooltip_posneg")
 	    .style("opacity", 0);
-
-
 
 	  posneg.svg.append("g")
 	      .attr("class", "x axis")
@@ -553,13 +709,13 @@ function plot_posneg() {
 	      .attr("width", function(d) { return posneg.xScale(d.x1) - posneg.xScale(d.x0); })
 	      .style("fill", function(d) { return posneg.color(d.name); });
 
-	  posneg.bars.append("text")
-	      .attr("x", function(d) { return posneg.xScale(d.x0); })
-	      .attr("y", posneg.yScale.rangeBand()/2)
-	      .attr("dy", "0.5em")
-	      .attr("dx", "0.5em")
-	      .style("font" ,"10px sans-serif")
-	      .style("text-anchor", "start")
+	  // posneg.bars.append("text")
+	  //     .attr("x", function(d) { return posneg.xScale(d.x0); })
+	  //     .attr("y", posneg.yScale.rangeBand()/2)
+	  //     .attr("dy", "0.5em")
+	  //     .attr("dx", "0.5em")
+	  //     .style("font" ,"10px sans-serif")
+	  //     .style("text-anchor", "start")
 	      // .text(function(d) { return d.N !== 0 && (d.x1-d.x0)>3 ? Math.abs(Math.floor(d.x1-d.x0)) + "%" : "" });
 
 	  posneg.vakken.insert("rect",":first-child")
@@ -614,27 +770,168 @@ function plot_posneg() {
 
 	  var movesize = posneg.width/2 - startp.node().getBBox().width/2;
 	  d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
-	});
+	// });
 }
 
+data_posneg = [];
 function plot(local_scatter,local_fingerprint,local_posneg,local_authors) {
 	pre_scatter(local_scatter);
 	pre_fingerprint(local_fingerprint);
 	pre_posneg(local_posneg);
 	d3.csv("scatter_data.csv", function(error, data) {
 		handle_scatter_data(data);
+		// data_posneg = data.slice(0)
+		handle_posneg_data(data);
 		/* gerando o dicionarios de autores */		
 		data.forEach(function(author) {
 			/* name = author.author */
-			dict[author.author] = author
+			dict[author.author] = author;
 		});
 		authors = Object.keys(dict);
 		plot_scatter(data);
 		plot_fingerprint(data[0]);
 		plot_authors(local_authors,authors);
+		plot_posneg(data.slice(1,7));
 	});
-	plot_posneg();
 }
+
+function update_posneg(author,rem) {
+
+
+	// posneg.tooltip = d3.select("body").append("div")
+	//     .attr("class", "tooltip tooltip_posneg")
+	//     .style("opacity", 0);
+
+
+
+	  posneg.svg.select(".x.axis")
+	  	.transition().duration(300)
+		.call(posneg.xAxis);
+
+	  posneg.svg.select(".y.axis")
+	  	.transition().duration(300)
+		.call(posneg.yAxis);
+
+
+
+	var bars = posneg.vakken.selectAll(".bar")
+	var data = bars.data();
+	
+	for (i=0; i<data.length; i++) {
+		if (data[i].author == author) {
+			if (rem) {
+				data.push(dict[author]);
+				break;
+			} else {
+				data.splice(i,1);
+			}
+		}
+	}
+
+	var min_val = d3.min(data, function(d) {
+	  return d.boxes["0"].x0;
+	  });
+
+	var max_val = d3.max(data, function(d) {
+	  return d.boxes["3"].x1;
+	  });
+
+	posneg.xScale.domain([min_val, max_val]).nice();
+	posneg.yScale.domain(data.map(function(d) { return d.author; }));
+
+	bars.data(function(d) { return d.boxes; })
+		.enter()
+		.append("g")
+		// .attr("class", "subbar")
+		.on("mouseover", function(d) {
+		   posneg.tooltip.transition()
+		       .duration(200)
+			.style("opacity", .9);
+		   posneg.tooltip.html("Authors: " + d.author + "<br>"
+				+ "Joy: " + d.joy + "<br>"
+				+ "Positive: " +  d.positive + "<br>"
+				+ "Negative: " + d.negative + "<br>"
+				+ "Sadness: " + d.sadness)
+			.style("left", (d3.event.pageX + 5) + "px")
+			.style("top", (d3.event.pageY - 28) + "px");
+		})
+		.on("mouseout", function(d) {
+		   posneg.tooltip.transition()
+			.duration(500)
+			.style("opacity", 0);
+		});
+
+	bars.append("rect")
+		.attr("height", posneg.yScale.rangeBand())
+		.attr("x", function(d) { return posneg.xScale(d.x0); })
+		.attr("width", function(d) { return posneg.xScale(d.x1) - posneg.xScale(d.x0); })
+		.style("fill", function(d) { return posneg.color(d.name); });
+
+	  // bars.append("text")
+	  //     .attr("x", function(d) { return posneg.xScale(d.x0); })
+	  //     .attr("y", posneg.yScale.rangeBand()/2)
+	  //     .attr("dy", "0.5em")
+	  //     .attr("dx", "0.5em")
+	  //     .style("font" ,"10px sans-serif")
+	  //     .style("text-anchor", "start")
+	      // .text(function(d) { return d.N !== 0 && (d.x1-d.x0)>3 ? Math.abs(Math.floor(d.x1-d.x0)) + "%" : "" });
+
+	  posneg.vakken.insert("rect",":first-child")
+	      .attr("height", posneg.yScale.rangeBand())
+	      .attr("x", "1")
+	      .attr("width", posneg.width)
+	      .attr("fill-opacity", "0.5")
+	      .style("fill", "#F5F5F5")
+	      .attr("class", function(d,index) { return index%2==0 ? "even" : "uneven"; });
+
+	  // posneg.svg.append("g")
+	  //     .attr("class", "y axis")
+	  // .append("line")
+	  //     .attr("x1", posneg.xScale(0))
+	  //     .attr("x2", posneg.xScale(0))
+	  //     .attr("y2", posneg.height);
+
+	  // var startp = posneg.svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
+	  // // this is not nice, we should calculate the bounding box and use that
+	  // var legend_tabs = [0, 120, 200, 375, 450];
+	  // posneg.legend = startp.selectAll(".legend")
+	  //     .data(posneg.color.domain().slice())
+	  //   .enter().append("g")
+	  //     .attr("class", "legend")
+	  //     .attr("transform", function(d, i) { return "translate(" + legend_tabs[i] + ",-45)"; });
+          //
+	  // posneg.legend.append("rect")
+	  //     .attr("x", 0)
+	  //     .attr("width", 18)
+	  //     .attr("height", 18)
+	  //     .style("fill", posneg.color);
+          //
+	  // posneg.legend.append("text")
+	  //     .attr("x", 22)
+	  //     .attr("y", 9)
+	  //     .attr("dy", ".35em")
+	  //     .style("text-anchor", "start")
+	  //     .style("font" ,"10px sans-serif")
+	  //     .text(function(d) { return d; });
+
+	  // d3.selectAll(".axis path")
+	  //     .style("fill", "none")
+	  //     .style("stroke", "#000")
+	  //     .style("shape-rendering", "crispEdges")
+          //
+	  // d3.selectAll(".axis line")
+	  //     .style("fill", "none")
+	  //     .style("stroke", "#000")
+	  //     .style("shape-rendering", "crispEdges")
+
+
+
+	  // var movesize = posneg.width/2 - startp.node().getBBox().width/2;
+	  // d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
+	// });
+
+}
+
 
 // var divs = new Object();
 function plot_authors(local,authors) {
@@ -655,10 +952,13 @@ function plot_authors(local,authors) {
 	// 			d + ">" + d;
 	// 	});
 
-	divs.append("input")
+	var checkboxes = divs.append("input")
 		.attr("type","checkbox")
 		.attr("name","author")
-		.attr("value",fun);
+		.attr("id",fun)
+		.on("change",function(d) {update_posneg(d,this.checked)});
 	divs.append("a").html(fun);
 	 // divs.append("a").html(fun);
+	 // checkboxes
+// d3.select("input").on("change", change);
 }
